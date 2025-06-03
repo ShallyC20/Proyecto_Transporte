9bf519d4ceb27d1f64588356119620925b169696
AnderesLaptop <andres10000fky@gmail.com>
2025-06-03 04:11:45 -0400

    Arreglos 1

--- js/conexionFireBase.js
+++ js/conexionFireBase.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyCPyRTvy7EEvAxs1dZjwKk68IR4R-ZPlbQ",
-  authDomain: "transportesgeolocalizacion.firebaseapp.com",
-  projectId: "transportesgeolocalizacion",
-  storageBucket: "transportesgeolocalizacion.firebasestorage.app",
-  messagingSenderId: "200393220762",
-  appId: "1:200393220762:web:0f9e9e1ed19b4ba5b4b523",
-  measurementId: "G-PN7FB0RTDM"
+    apiKey: "AIzaSyCPyRTvy7EEvAxs1dZjwKk68IR4R-ZPlbQ",
+    authDomain: "transportesgeolocalizacion.firebaseapp.com",
+    projectId: "transportesgeolocalizacion",
+    storageBucket: "transportesgeolocalizacion.firebasestorage.app",
+    messagingSenderId: "200393220762",
+    appId: "1:200393220762:web:0f9e9e1ed19b4ba5b4b523",
+    measurementId: "G-PN7FB0RTDM"
 };
 
 // Inicia el Firebase
